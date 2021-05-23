3c380bc1fa1eb4b7a1463949408a840d00a82901
erdldncr <erdldncr@gmail.com>
2021-05-23 11:24:57 +0100

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
+     
     </div>
   );
 }
