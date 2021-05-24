806a4f223cd538e12c7fb2ac8de56429975594b9
erdldncr <erdldncr@gmail.com>
2021-05-24 12:09:27 +0100

    project-completed

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import './Tour.scss'
 export default class Tour extends Component {
+    state={
+        showInfo:false
+    }
     render() {
+        const {id,img,city,name,info,deleteTour}=this.props
         return (
             <article className='tour'>
                 <div className="img-container">
-                    <img width='200px' src='/logo512.png' alt=""/>
-                    <span className="close-btn">
+                    <img  src={img} alt=""/>
+                    <span onClick={()=>deleteTour(id)} className="close-btn">
                         <i className="fas fa-window-close"></i>
                     </span>
                 </div>
                 <div className="tour-info">
-                    <h3>city</h3>
-                    <h3>name</h3>
-                    <h5>info <span> <i className="fas fa-caret-square-down"></i> </span></h5>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, laboriosam?</p>
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>info <span> <i onClick={()=>this.setState({showInfo:!this.state.showInfo})} className="fas fa-caret-square-down"></i> </span></h5>
+                    {
+                    this.state.showInfo&&<p>{info}</p>
+                    }
+                    
                 </div>
             </article>
         )
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 import './TourList.scss'
 import Tour from '../Tour/Tour'
+import {tourData} from '../../tourData'
 export default class TourList extends Component {
+    state={
+        tours:tourData
+    }
+    deleteTour=(id)=>{
+        
+    this.setState({tours:this.state.tours.filter(tour=>tour.id!==id)})
+    }
     render() {
         return (
             <section className='tourlist' > 
-                <Tour/>
+                {
+                    this.state.tours.map(
+                        tour=><Tour deleteTour={this.deleteTour} key={tour.id} {...tour}/>
+                    )
+                }
             </section>
         )
     }
